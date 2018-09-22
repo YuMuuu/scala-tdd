326c13f2f504ce42069d36821319b5f1127d8f3f
YuMuuu <c01150788f@edu.teu.ac.jp>
2018-09-22 23:52:12 +0900

    fin 第3章:

--- money/src/test/scala/MoneyTest.scala
+++ money/src/test/scala/MoneyTest.scala
@@ -2,10 +2,16 @@ import org.scalatest._
 
 class MoneyTest extends FlatSpec with DiagrammedAssertions {
 
-  "Multiplication" should "$5 * 2" in {
+  "Multiplication1" should "$5 * 2" in {
     val five: Dollar = Dollar(5)
     five.times(2)
     assert(five.amount == 10)
+  }
+
+  "Multiplication2" should "$5 * 3" in {
+    val five: Dollar = Dollar(5)
+    five.times(3)
+    assert(five.amount == 15)
   }
 
 }
